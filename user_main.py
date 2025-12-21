cde80f41f7958c128f77ce3dac9fc4cc116eb950
equality-ops <2723242439@qq.com>
2025-12-21 21:17:48 +0800

    停止定时器以防止异常退出测试程序

--- user_main.py
+++ user_main.py
@@ -120,6 +120,8 @@ while True:
     # 这么做是为了防止写错代码导致异常 有一个退出的手段
     if switch2.value() != state2:
         print("Test program stop.")
+        ticker(1).stop()
+        ticker(2).stop()
         break
 
     gc.collect()
