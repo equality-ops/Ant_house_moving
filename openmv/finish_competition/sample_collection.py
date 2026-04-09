ca0d219b80b75e12444053c21759b6b663fe6110
LilAn09 <1327161394@qq.com>
2026-04-09 20:33:31 +0800

    调整图像捕获设置，修改保存图像的起始编号

--- openmv/finish_competition/sample_collection.py
+++ openmv/finish_competition/sample_collection.py
@@ -18,23 +18,26 @@ sensor.set_auto_gain(False)  # 关闭自动增益
 sensor.set_auto_whitebal(False)  # 关闭自动白平衡
 sensor.set_brightness(800)
 sensor.set_contrast(2) # 对比度
+sensor.set_vflip(True)
+sensor.skip_frames(time=200)  # 跳过初始帧，让摄像头稳定
+sensor.set_hmirror(True)
 sensor.skip_frames(time=200)  # 跳过初始帧，让摄像头稳定
 clock = time.clock()
 
 
-save_img_num = 1647
+save_img_num = 2130
 count = -100
 
 while(True):
     img = sensor.snapshot()             # 获取一幅图像
     # blue.toggle()                       # 蓝灯翻转
-    
+
     count += 1
-    
+
     if (count % 20) == 0 and count > 0:
         # 修改文件名称，准备保存
         save_img_num += 1
         image_pat = "/sd/picture/"+str(save_img_num)+".jpg"
-    
+
         # 将拷贝之后的图像保存到sd卡
         img.save(image_pat,quality=99)
