9dc275342b4017a13e46163ef5fb29b63661bb98
equality-ops <2723242439@qq.com>
2026-04-11 20:35:50 +0800

    Add camera functionality and update main application
    - Introduced a new source file `camera.c` for camera operations.
    - Updated project configuration files to include `camera.c`.
    - Added a breakpoint in the debugger settings for easier debugging.
    - Enhanced the main application to initialize and display camera images on the IPS module.
    - Included detailed comments and instructions for hardware connections and common troubleshooting steps.

--- IMU/IMU/libraries/zf_device/zf_device_wifi_spi.h
+++ IMU/IMU/libraries/zf_device/zf_device_wifi_spi.h
@@ -1,50 +1,50 @@
 /*********************************************************************************************************************
-* STC32G144K Opensourec Library ����STC32G144K ��Դ�⣩��һ�����ڹٷ� SDK �ӿڵĵ�������Դ��
-* Copyright (c) 2025 SEEKFREE ��ɿƼ�
+* STC32G144K Opensourec Library 即（STC32G144K 开源库）是一个基于官方 SDK 接口的第三方开源库
+* Copyright (c) 2025 SEEKFREE 逐飞科技
 *
-* ���ļ���STC32G144K��Դ���һ����
+* 本文件是STC32G144K开源库的一部分
 *
-* STC32G144K ��Դ�� ���������
-* �����Ը���������������ᷢ���� GPL��GNU General Public License���� GNUͨ�ù�������֤��������
-* �� GPL �ĵ�3�棨�� GPL3.0������ѡ��ģ��κκ����İ汾�����·�����/���޸���
+* STC32G144K 开源库 是免费软件
+* 您可以根据自由软件基金会发布的 GPL（GNU General Public License，即 GNU通用公共许可证）的条款
+* 即 GPL 的第3版（即 GPL3.0）或（您选择的）任何后来的版本，重新发布和/或修改它
 *
-* ����Դ��ķ�����ϣ�����ܷ������ã�����δ�������κεı�֤
-* ����û�������������Ի��ʺ��ض���;�ı�֤
-* ����ϸ����μ� GPL
+* 本开源库的发布是希望它能发挥作用，但并未对其作任何的保证
+* 甚至没有隐含的适销性或适合特定用途的保证
+* 更多细节请参见 GPL
 *
-* ��Ӧ�����յ�����Դ���ͬʱ�յ�һ�� GPL �ĸ���
-* ���û�У������<https://www.gnu.org/licenses/>
+* 您应该在收到本开源库的同时收到一份 GPL 的副本
+* 如果没有，请参阅<https://www.gnu.org/licenses/>
 *
-* ����ע����
-* ����Դ��ʹ�� GPL3.0 ��Դ����֤Э�� ������������Ϊ���İ汾
-* ��������Ӣ�İ��� libraries/doc �ļ����µ� GPL3_permission_statement.txt �ļ���
-* ����֤������ libraries �ļ����� �����ļ����µ� LICENSE �ļ�
-* ��ӭ��λʹ�ò����������� ���޸�����ʱ���뱣����ɿƼ��İ�Ȩ����������������
+* 额外注明：
+* 本开源库使用 GPL3.0 开源许可证协议 以上许可申明为译文版本
+* 许可申明英文版在 libraries/doc 文件夹下的 GPL3_permission_statement.txt 文件中
+* 许可证副本在 libraries 文件夹下 即该文件夹下的 LICENSE 文件
+* 欢迎各位使用并传播本程序 但修改内容时必须保留逐飞科技的版权声明（即本声明）
 *
-* �ļ�����          
-* ��˾����          �ɶ���ɿƼ����޹�˾
-* �汾��Ϣ          �鿴 libraries/doc �ļ����� version �ļ� �汾˵��
-* ��������          MDK FOR C251
-* ����ƽ̨          STC32G144K
-* ��������          https://seekfree.taobao.com/
+* 文件名称          
+* 公司名称          成都逐飞科技有限公司
+* 版本信息          查看 libraries/doc 文件夹内 version 文件 版本说明
+* 开发环境          MDK FOR C251
+* 适用平台          STC32G144K
+* 店铺链接          https://seekfree.taobao.com/
 *
-* �޸ļ�¼
-* ����              ����           ��ע
-* 2025-11-20        ��W            first version
+* 修改记录
+* 日期              作者           备注
+* 2025-11-20        大W            first version
 ********************************************************************************************************************/
 /*********************************************************************************************************************
-* ���߶��壺
+* 接线定义：
 *                   ------------------------------------
-*                   ģ��ܽ�            ��Ƭ���ܽ�
-*                   RST                 �鿴 zf_device_wifi_spi.h �� WIFI_SPI_RST_PIN �궨��
-*                   INT                 �鿴 zf_device_wifi_spi.h �� WIFI_SPI_INT_PIN �궨��
-*                   CS                  �鿴 zf_device_wifi_spi.h �� WIFI_SPI_CS_PIN �궨��
-*                   MISO                �鿴 zf_device_wifi_spi.h �� WIFI_SPI_MISO_PIN �궨��
-*                   SCK                 �鿴 zf_device_wifi_spi.h �� WIFI_SPI_SCK_PIN �궨��
-*                   MOSI                �鿴 zf_device_wifi_spi.h �� WIFI_SPI_MOSI_PIN �궨��
-*                   5V                  5V ��Դ
-*                   GND                 ��Դ��
-*                   ������������
+*                   模块管脚            单片机管脚
+*                   RST                 查看 zf_device_wifi_spi.h 中 WIFI_SPI_RST_PIN 宏定义
+*                   INT                 查看 zf_device_wifi_spi.h 中 WIFI_SPI_INT_PIN 宏定义
+*                   CS                  查看 zf_device_wifi_spi.h 中 WIFI_SPI_CS_PIN 宏定义
+*                   MISO                查看 zf_device_wifi_spi.h 中 WIFI_SPI_MISO_PIN 宏定义
+*                   SCK                 查看 zf_device_wifi_spi.h 中 WIFI_SPI_SCK_PIN 宏定义
+*                   MOSI                查看 zf_device_wifi_spi.h 中 WIFI_SPI_MOSI_PIN 宏定义
+*                   5V                  5V 电源
+*                   GND                 电源地
+*                   其余引脚悬空
 *                   ------------------------------------
 *********************************************************************************************************************/
 
@@ -54,105 +54,105 @@
 #include "zf_common_typedef.h"
 
           
-#define WIFI_SPI_INDEX              ( SPI_2 )                   // ����ʹ�õ�SPI��
-#define WIFI_SPI_SPEED              ( 48 * 1000 * 1000 )        // Ӳ�� SPI ����
-#define WIFI_SPI_SCK_PIN            ( SPI2_CH4_SCLK_P83 )       // ����SPI_SCK����
-#define WIFI_SPI_MOSI_PIN           ( SPI2_CH4_MOSI_P81 )       // ����SPI_MOSI����
-#define WIFI_SPI_MISO_PIN           ( SPI2_CH4_MISO_P82 )       // ����SPI_MISO����  IPSû��MISO���ţ�����������Ȼ��Ҫ���壬��spi�ĳ�ʼ��ʱ��Ҫʹ��
-#define WIFI_SPI_CS_PIN             ( IO_P64 )                  // ����SPI_CS���� ��������CS����
-#define WIFI_SPI_INT_PIN            ( IO_P15 )                  // �����ж�����
-#define WIFI_SPI_RST_PIN            ( IO_P47 )                  // ���帴λ����
+#define WIFI_SPI_INDEX              ( SPI_2 )                   // 定义使用的SPI号
+#define WIFI_SPI_SPEED              ( 48 * 1000 * 1000 )        // 硬件 SPI 速率
+#define WIFI_SPI_SCK_PIN            ( SPI2_CH4_SCLK_P83 )       // 定义SPI_SCK引脚
+#define WIFI_SPI_MOSI_PIN           ( SPI2_CH4_MOSI_P81 )       // 定义SPI_MOSI引脚
+#define WIFI_SPI_MISO_PIN           ( SPI2_CH4_MISO_P82 )       // 定义SPI_MISO引脚  IPS没有MISO引脚，但是这里任然需要定义，在spi的初始化时需要使用
+#define WIFI_SPI_CS_PIN             ( IO_P64 )                  // 定义SPI_CS引脚 采用软件CS引脚
+#define WIFI_SPI_INT_PIN            ( IO_P15 )                  // 定义中断引脚
+#define WIFI_SPI_RST_PIN            ( IO_P47 )                  // 定义复位引脚
 
 #define WIFI_SPI_CS(x)              ( P64  = x )
 
-#define WIFI_SPI_RECVIVE_FIFO_SIZE  (1024)                      // ����FIFO��С
-#define WIFI_SPI_READ_TRANSFER      (1)                         // �ڵ���wifi_spi_read_buffer �Ƿ��Է���SPIͨѶ�����ģ�����Ƿ���������Ҫ��ȡ 1������SPIͨѶ 0��������SPIͨѶ������ȡFIFO 
-                                                                // ���Ӧ�ó�����û���κεĵط����÷��ͺ�������WIFI_SPI_READ_TRANSFER��������Ϊ1
+#define WIFI_SPI_RECVIVE_FIFO_SIZE  (1024)                      // 接收FIFO大小
+#define WIFI_SPI_READ_TRANSFER      (1)                         // 在调用wifi_spi_read_buffer 是否尝试发起SPI通讯来检测模块内是否有数据需要读取 1：发起SPI通讯 0：不发起SPI通讯，仅读取FIFO 
+                                                                // 如果应用程序中没有任何的地方调用发送函数，则WIFI_SPI_READ_TRANSFER必须设置为1
                                                                 
-#define WIFI_SPI_AUTO_CONNECT       (0)                         // �����Ƿ��ʼ��ʱ����TCP����UDP����    0-���Զ�����  1-�Զ�����TCP������  2-�Զ�����UDP
+#define WIFI_SPI_AUTO_CONNECT       (0)                         // 定义是否初始化时建立TCP或者UDP连接    0-不自动连接  1-自动连接TCP服务器  2-自动连接UDP
 
 #if     (WIFI_SPI_AUTO_CONNECT > 2)    
-#error "WIFI_SPI_AUTO_CONNECT ��ֵֻ��Ϊ [0,1,2]" 
+#error "WIFI_SPI_AUTO_CONNECT 的值只能为 [0,1,2]" 
 #else   
-#define WIFI_SPI_TARGET_IP          "192.168.2.49"              // ����Ŀ��� IP,��Ҫ�пո񣡣���
-#define WIFI_SPI_TARGET_PORT        "8086"                      // ����Ŀ��Ķ˿�
-#define WIFI_SPI_LOCAL_PORT         "8086"                      // �����Ķ˿� 0�����  �����÷�Χ2048-65535  Ĭ�� 6666
+#define WIFI_SPI_TARGET_IP          "192.168.43.7"              // 连接目标的 IP,不要有空格！！！
+#define WIFI_SPI_TARGET_PORT        "8086"                      // 连接目标的端口
+#define WIFI_SPI_LOCAL_PORT         "8086"                      // 本机的端口 0：随机  可设置范围2048-65535  默认 6666
 #endif
 
 
-#define WIFI_SPI_RECVIVE_SIZE       (32)                        // ÿ��SPI������յ��ֽ��� �������޸�
-#define WIFI_SPI_TRANSFER_SIZE      (4088)                      // ���SPI������յ��ֽ��� �������޸�
+#define WIFI_SPI_RECVIVE_SIZE       (32)                        // 每次SPI传输接收的字节数 不允许修改
+#define WIFI_SPI_TRANSFER_SIZE      (4088)                      // 最大SPI传输接收的字节数 不允许修改
 
 
 
 typedef enum
 {
-    // �������͵�����
-    WIFI_SPI_INVALID1               = 0x00,                     // ��Ч���ݰ�
-    WIFI_SPI_RESET                  = 0x01,                     // ��λ����
-    WIFI_SPI_DATA                   = 0x02,                     // ͸�����ݰ�
-    WIFI_SPI_UDP_SEND               = 0x03,                     // UDP��������������,Ĭ��SPI�������ݺ�2MSδ�յ������Զ���������
-    WIFI_SPI_CLOSE_SOCKET           = 0x04,                     // �Ͽ�����
+    // 主机发送的命令
+    WIFI_SPI_INVALID1               = 0x00,                     // 无效数据包
+    WIFI_SPI_RESET                  = 0x01,                     // 复位命令
+    WIFI_SPI_DATA                   = 0x02,                     // 透传数据包
+    WIFI_SPI_UDP_SEND               = 0x03,                     // UDP下立即发送命令,默认SPI接收数据后2MS未收到数据自动发送数据
+    WIFI_SPI_CLOSE_SOCKET           = 0x04,                     // 断开连接
                 
-    WIFI_SPI_SET_WIFI_INFORMATION   = 0x10,                     // ����WIFI��Ϣ����
-    WIFI_SPI_SET_SOCKET_INFORMATION = 0x11,                     // ����SOCKET��Ϣ����
-    WIFI_SPI_SET_WIFI_SCAN          = 0x12,                     // ��ʼɨ��WIFI
-    WIFI_SPI_SET_READ_LENGTH        = 0x13,                     // ��������ȡ����
+    WIFI_SPI_SET_WIFI_INFORMATION   = 0x10,                     // 设置WIFI信息命令
+    WIFI_SPI_SET_SOCKET_INFORMATION = 0x11,                     // 设置SOCKET信息命令
+    WIFI_SPI_SET_WIFI_SCAN          = 0x12,                     // 开始扫描WIFI
+    WIFI_SPI_SET_READ_LENGTH        = 0x13,                     // 设置最大读取长度
                     
-    WIFI_SPI_GET_VERSION            = 0x20,                     // ��ȡģ��汾
-    WIFI_SPI_GET_MAC_ADDR           = 0x21,                     // ��ȡģ��MAC��ַ
-    WIFI_SPI_GET_IP_ADDR            = 0x22,                     // ��ȡģ��IP��ַ
-    WIFI_SPI_GET_TIME1              = 0x23,                     // ��ȡʱ�� ��ʽ1
-    WIFI_SPI_GET_TIME2              = 0x24,                     // ��ȡʱ�� ��ʽ2
-    WIFI_SPI_GET_TIME3              = 0x25,                     // ��ȡʱ�� ��ʽ3
+    WIFI_SPI_GET_VERSION            = 0x20,                     // 获取模块版本
+    WIFI_SPI_GET_MAC_ADDR           = 0x21,                     // 获取模块MAC地址
+    WIFI_SPI_GET_IP_ADDR            = 0x22,                     // 获取模块IP地址
+    WIFI_SPI_GET_TIME1              = 0x23,                     // 获取时间 格式1
+    WIFI_SPI_GET_TIME2              = 0x24,                     // 获取时间 格式2
+    WIFI_SPI_GET_TIME3              = 0x25,                     // 获取时间 格式3
                     
-    // �ӻ��ش�������                  
-    WIFI_SPI_REPLY_OK               = 0x80,                     // �ӻ�Ӧ�����ȷ����
-    WIFI_SPI_REPLY_ERROR            = 0x81,                     // �ӻ�Ӧ��Ĵ�������
+    // 从机回传的命令                  
+    WIFI_SPI_REPLY_OK               = 0x80,                     // 从机应答的正确命令
+    WIFI_SPI_REPLY_ERROR            = 0x81,                     // 从机应答的错误命令
                     
-    WIFI_SPI_REPLY_DATA_START       = 0x90,                     // �ӻ��ش������ݰ������һ���������Ҫ������ȡ
-    WIFI_SPI_REPLY_DATA_END         = 0x91,                     // �ӻ��ش������ݰ��������Ѷ�ȡ���
+    WIFI_SPI_REPLY_DATA_START       = 0x90,                     // 从机回传的数据包，并且还有数据需要主机读取
+    WIFI_SPI_REPLY_DATA_END         = 0x91,                     // 从机回传的数据包，数据已读取完毕
                     
-    WIFI_SPI_REPLY_VERSION          = 0xA0,                     // �ӻ��ظ��̼��汾
-    WIFI_SPI_REPLY_MAC_ADDR         = 0xA1,                     // �ӻ��ظ�����MAC��ַ����Ϣ
-    WIFI_SPI_REPLY_IP_ADDR          = 0xA2,                     // �ӻ��ظ�����IP��ַ���˿ں�
-    WIFI_SPI_REPLY_TIME1            = 0xA3,                     // �ӻ��ظ�ʱ��
-    WIFI_SPI_REPLY_TIME2            = 0xA4,                     // �ӻ��ظ�ʱ��
-    WIFI_SPI_REPLY_TIME3            = 0xA5,                     // �ӻ��ظ�ʱ��
-    WIFI_SPI_INVALID2               = 0xFF                      // ��Ч���ݰ�
+    WIFI_SPI_REPLY_VERSION          = 0xA0,                     // 从机回复固件版本
+    WIFI_SPI_REPLY_MAC_ADDR         = 0xA1,                     // 从机回复本机MAC地址等信息
+    WIFI_SPI_REPLY_IP_ADDR          = 0xA2,                     // 从机回复本机IP地址、端口号
+    WIFI_SPI_REPLY_TIME1            = 0xA3,                     // 从机回复时间
+    WIFI_SPI_REPLY_TIME2            = 0xA4,                     // 从机回复时间
+    WIFI_SPI_REPLY_TIME3            = 0xA5,                     // 从机回复时间
+    WIFI_SPI_INVALID2               = 0xFF                      // 无效数据包
 }wifi_spi_packets_command_enum;             
                 
 typedef enum                
 {               
-    WIFI_SPI_IDLE,                                              // ģ����У����Խ���SPIͨѶ
-    WIFI_SPI_BUSY,                                              // ģ����æ�����ɽ���SPIͨѶ
+    WIFI_SPI_IDLE,                                              // 模块空闲，可以进行SPI通讯
+    WIFI_SPI_BUSY,                                              // 模块正忙，不可进行SPI通讯
 }wifi_spi_state_enum;               
                 
                 
 typedef struct              
 {               
-    uint8   command;                                            // ������
-    uint8   reserve;                                            // ����
-    uint16  length;                                             // ����Ч����
+    uint8   command;                                            // 命令字
+    uint8   reserve;                                            // 保留
+    uint16  length;                                             // 包有效长度
 }wifi_spi_head_struct;              
                 
                 
 typedef struct 
 {               
-    wifi_spi_head_struct  head;                                 // ֡ͷ
-    uint8 buffer[WIFI_SPI_RECVIVE_SIZE];                        // ������
+    wifi_spi_head_struct  head;                                 // 帧头
+    uint8 buffer[WIFI_SPI_RECVIVE_SIZE];                        // 缓冲区
 }wifi_spi_packets_struct;               
                 
 typedef enum
 {
-    WIFI_SPI_UTC_0 = 1,                                         // ����ʱ��
-    WIFI_SPI_GMT,                                               // ����ʱ�� ת��ΪGMT��ʽ����׺����GMT һ�����ڶԽ��ƶ˴�ģ��ʹ��
-    WIFI_SPI_UTC_8,                                             // ����ʱ��
+    WIFI_SPI_UTC_0 = 1,                                         // 世界时间
+    WIFI_SPI_GMT,                                               // 世界时间 转换为GMT格式，后缀增加GMT 一般用于对接云端大模型使用
+    WIFI_SPI_UTC_8,                                             // 北京时间
 }wifi_spi_time_enum;
 
-extern char wifi_spi_version[12];                               // �̼��汾         �ַ���
-extern char wifi_spi_mac_addr[20];                              // ģ��MAC��ַ      �ַ���
-extern char wifi_spi_ip_addr_port[25];                          // IP��ַ��˿ں�   �ַ���
+extern char wifi_spi_version[12];                               // 固件版本         字符串
+extern char wifi_spi_mac_addr[20];                              // 模块MAC地址      字符串
+extern char wifi_spi_ip_addr_port[25];                          // IP地址与端口号   字符串
 
 uint8   wifi_spi_get_time           (wifi_spi_time_enum time_format, char *buffer, uint8 buffer_size);
 uint8   wifi_spi_wifi_scan          (char *buffer, uint16 buffer_size);
--- IMU/IMU/project/user/main.c
+++ IMU/IMU/project/user/main.c
@@ -1,86 +1,146 @@
+/*********************************************************************************************************************
+* STC32G144K Opensourec Library 即（STC32G144K 开源库）是一个基于官方 SDK 接口的第三方开源库
+* Copyright (c) 2025 SEEKFREE 逐飞科技
+*
+* 本文件是STC32G144K开源库的一部分
+*
+* STC32G144K 开源库 是免费软件
+* 您可以根据自由软件基金会发布的 GPL（GNU General Public License，即 GNU通用公共许可证）的条款
+* 即 GPL 的第3版（即 GPL3.0）或（您选择的）任何后来的版本，重新发布和/或修改它
+*
+* 本开源库的发布是希望它能发挥作用，但并未对其作任何的保证
+* 甚至没有隐含的适销性或适合特定用途的保证
+* 更多细节请参见 GPL
+*
+* 您应该在收到本开源库的同时收到一份 GPL 的副本
+* 如果没有，请参阅<https://www.gnu.org/licenses/>
+*
+* 额外注明：
+* 本开源库使用 GPL3.0 开源许可证协议 以上许可申明为译文版本
+* 许可申明英文版在 libraries/doc 文件夹下的 GPL3_permission_statement.txt 文件中
+* 许可证副本在 libraries 文件夹下 即该文件夹下的 LICENSE 文件
+* 欢迎各位使用并传播本程序 但修改内容时必须保留逐飞科技的版权声明（即本声明）
+*
+* 文件名称          
+* 公司名称          成都逐飞科技有限公司
+* 版本信息          查看 libraries/doc 文件夹内 version 文件 版本说明
+* 开发环境          MDK FOR C251
+* 适用平台          STC32G144K
+* 店铺链接          https://seekfree.taobao.com/
+*
+* 修改记录
+* 日期              作者           备注
+* 2025-11-20        大W            first version
+********************************************************************************************************************/
 #include "zf_common_headfile.h"
-#include "quaternion.h"
 
-#define LED1                        (IO_P52)
-#define PIT                         (TIM0_PIT)
-#define GYRO_LSB_PER_DPS   (32768.0f / 2000.0f)
-#define RAD_PER_DEG        (3.1415f / 180.0f)
-#define DT                 0.01f
 
-static Quat q;
-static Vec3 forward_body;
-static int tick_count = 0;
-static float roll_angle = 0.0f;
-static float pitch_angle = 0.0f;
-static float yaw_angle = 0.0f;
-static float yaw_offset = 0.0f;
-float gyro_bias_x = 0.0f, gyro_bias_y = 0.0f, gyro_bias_z = 0.0f;
+// *************************** 例程硬件连接说明 ***************************
+//
+// *************************** 例程硬件连接说明 ***************************
+//
+// 接入2寸IPS模块
+//      模块管脚            单片机管脚
+//      双排排针 并口两寸屏 硬件引脚
+//      RD                  查看 zf_device_ips200_parallel8.h 中 IPS200_RD_PIN 宏定义 默认 B13
+//      WR                  查看 zf_device_ips200_parallel8.h 中 IPS200_WR_PIN 宏定义 默认 B15
+//      RS                  查看 zf_device_ips200_parallel8.h 中 IPS200_RS_PIN 宏定义 默认 B7
+//      RST                 查看 zf_device_ips200_parallel8.h 中 IPS200_RST_PIN宏定义 默认 D7
+//      CS                  查看 zf_device_ips200_parallel8.h 中 IPS200_CS_PIN 宏定义 默认 D4
+//      BL                  查看 zf_device_ips200_parallel8.h 中 IPS200_BL_PIN 宏定义 默认 D0
+//      D0-D7               查看 zf_device_ips200_parallel8.h 中 IPS200_Dx_PIN 宏定义 默认 E0-E7
+//      GND                 核心板电源地 GND
+//      3V3                 核心板 3V3 电源
+//      单排排针 SPI 两寸屏 硬件引脚
+//      SCL                 查看 zf_device_ips200.h 中 IPS200_SCL_PIN_SPI  宏定义 默认 B13
+//      SDA                 查看 zf_device_ips200.h 中 IPS200_SDA_PIN_SPI  宏定义 默认 B15
+//      RST                 查看 zf_device_ips200.h 中 IPS200_RST_PIN_SPI  宏定义 默认 B7
+//      DC                  查看 zf_device_ips200.h 中 IPS200_DC_PIN_SPI   宏定义 默认 D7
+//      CS                  查看 zf_device_ips200.h 中 IPS200_CS_PIN_SPI   宏定义 默认 D4
+//      BL                  查看 zf_device_ips200.h 中 IPS200_BLk_PIN_SPI  宏定义 默认 D0
+//      GND                 核心板电源地 GND                                      
+//      3V3                 核心板 3V3 电源
+//
+//
+// 接入总钻风灰度数字摄像头 对应主板摄像头接口 请注意线序
+//      模块管脚            单片机管脚
+//      TXD                 查看 zf_device_mt9v03x.h 中 MT9V03X_COF_UART_TX 宏定义 默认 D2  摄像头 UART-TX 引脚 要接在单片机 RX 上
+//      RXD                 查看 zf_device_mt9v03x.h 中 MT9V03X_COF_UART_RX 宏定义 默认 C12 摄像头 UART-RX 引脚 要接在单片机 TX 上
+//      D0                  查看 zf_device_mt9v03x.h 中 MT9V03X_D0_PIN      宏定义 默认 A9
+//      D1                  查看 zf_device_mt9v03x.h 中 MT9V03X_D1_PIN      宏定义 默认 A10
+//      D2                  查看 zf_device_mt9v03x.h 中 MT9V03X_D2_PIN      宏定义 默认 C8 
+//      D3                  查看 zf_device_mt9v03x.h 中 MT9V03X_D3_PIN      宏定义 默认 C9 
+//      D4                  查看 zf_device_mt9v03x.h 中 MT9V03X_D4_PIN      宏定义 默认 C11
+//      D5                  查看 zf_device_mt9v03x.h 中 MT9V03X_D5_PIN      宏定义 默认 B6 
+//      D6                  查看 zf_device_mt9v03x.h 中 MT9V03X_D6_PIN      宏定义 默认 B8 
+//      D7                  查看 zf_device_mt9v03x.h 中 MT9V03X_D7_PIN      宏定义 默认 B9 
+//      PCLK                查看 zf_device_mt9v03x.h 中 MT9V03X_PCLK_PIN    宏定义 默认 A6
+//      VSYNC               查看 zf_device_mt9v03x.h 中 MT9V03X_VSY_PIN     宏定义 默认 A5 
+//      HSYNC               查看 zf_device_mt9v03x.h 中 MT9V03X_HERF_PIN    宏定义 默认 A4 
 
-void init_attitude(void) {
-    q.w = 1.0f; q.x = 0.0f; q.y = 0.0f; q.z = 0.0f;
-    forward_body.x = 1.0f; forward_body.y = 0.0f; forward_body.z = 0.0f;
-}
+// *************************** 例程测试说明 ***************************
+// 1.核心板烧录完成本例程 将核心板插在主板上 插到底
+//
+// 2.摄像头接在主板的摄像头接口 注意线序2寸IPS模块插入主板屏幕接口
+//
+// 3.主板上电 或者核心板链接完毕后上电 核心板按下复位按键
+//
+// 4.屏幕会显示初始化信息然后显示摄像头图像
+//
+// 如果发现现象与说明严重不符 请参照本文件最下方 例程常见问题说明 进行排查
 
-void update_attitude(void) {
-    float gx_raw, gy_raw, gz_raw;
-    float gx_dps, gy_dps, gz_dps;
-    float wx, wy, wz;
-    Quat dq;
-    Vec3 direction_pure;
-    Quat temp_q; // 用于暂存四元数乘法结果，更安全
+// **************************** 代码区域 ****************************
+#define LED1                    (IO_P52 )
 
-    imu660rb_get_gyro();
-    gx_raw = (float)imu660rb_gyro_x;
-    gy_raw = (float)imu660rb_gyro_y;
-    gz_raw = (float)imu660rb_gyro_z;
+void main(void)
+{
+    clock_init(SYSTEM_CLOCK_96M); 				// 时钟配置及系统初始化<务必保留>
+    debug_init();                       		// 调试串口信息初始化
+	gpio_init(LED1, GPO, 0, GPO_PUSH_PULL);
 
-    gx_dps = (gx_raw - gyro_bias_x) / (-GYRO_LSB_PER_DPS);
-    gy_dps = (gy_raw - gyro_bias_y) / (-GYRO_LSB_PER_DPS);
-    gz_dps = (gz_raw - gyro_bias_z) / (-GYRO_LSB_PER_DPS);
-
-    wx = gx_dps * RAD_PER_DEG;
-    wy = gy_dps * RAD_PER_DEG;
-    wz = gz_dps * RAD_PER_DEG;
-
-    // 使用指针传参更新四元数
-    omega_to_dq(&dq, wx, wy, wz, DT);
-    
-    // 计算结果存放到 temp_q，再赋回给 q
-    quat_mul(&temp_q, &q, &dq);
-    q = temp_q;
-    
-    quat_normalize(&q, &q); // 我们在底层做了保护，这里原地修改是安全的
-
-    rotate_vector_by_quat(&direction_pure, &q, &forward_body);  // 可选用
-    quat_to_euler(&q, &roll_angle, &pitch_angle, &yaw_angle);
-    yaw_angle += yaw_offset;
-    tick_count++;
-}
-
-void pit_hanlder(void) {
-    update_attitude();
-}
-
-void main(void) {
-    clock_init(SYSTEM_CLOCK_96M);
-    debug_init();
-    gpio_init(LED1, GPO, GPIO_HIGH, GPO_PUSH_PULL);
-
-    while(1) {
-        if(imu660rb_init())
-            printf("\r\nimu660rb init error.");
+    // 此处编写用户代码 例如外设初始化代码等
+    ips200_init();
+    ips200_show_string(0, 0, "mt9v03x init.");
+    while(1)
+    {
+        system_delay_ms(100);   
+        if(mt9v03x_init())
+            ips200_show_string(0, 16, "mt9v03x reinit.");
         else
             break;
-        gpio_toggle_level(LED1);
-        system_delay_ms(300);
+		gpio_toggle_level(LED1);                                 // 短延时快速闪灯表示异常
     }
+    ips200_show_string(0, 16, "init success.");
+    // 此处编写用户代码 例如外设初始化代码等
 
-    init_attitude();
-    pit_ms_init(PIT, 10, pit_hanlder);
-
-    while(1) {
-        printf("\r\nroll: %6.2f, pitch: %6.2f, yaw: %6.2f", roll_angle, pitch_angle, yaw_angle);
-        gpio_toggle_level(LED1);
-        system_delay_ms(300);
+    while(1)
+    {
+        // 此处编写需要循环执行的代码
+        ips200_displayimage03x(mt9v03x_image[0], MT9V03X_W, MT9V03X_H);
+        /*
+        if(mt9v03x_finish_flag)
+        {
+            ips200_displayimage03x(mt9v03x_image[0], MT9V03X_W, MT9V03X_H);
+            mt9v03x_finish_flag = 0;
+        }
+        */
+        // 此处编写需要循环执行的代码
     }
 }
+// **************************** 代码区域 ****************************
+
+// *************************** 例程常见问题说明 ***************************
+// 遇到问题时请按照以下问题检查列表检查
+//
+// 问题1：屏幕不显示
+//      如果使用主板测试，主板必须要用电池供电 检查屏幕供电引脚电压
+//      检查屏幕是不是插错位置了 检查引脚对应关系
+//      如果对应引脚都正确 检查一下是否有引脚波形不对 需要有示波器
+//      无法完成波形测试则复制一个GPIO例程将屏幕所有IO初始化为GPIO翻转电平 看看是否受控
+//
+// 问题2：显示 reinit 字样
+//      检查接线是否正常
+//      主板供电是否使用电量充足的电池供电
+//
+// 问题2：显示图像杂乱 错位
+//      检查摄像头信号线是否有松动
