c1544499645432af1a4d97785159f4a0dd8e447f
equality-ops <2723242439@qq.com>
2026-07-14 17:22:51 +0800

    优化视觉伺服控制逻辑，移除不必要的目标坐标更新

--- main_car/ant_vision.py
+++ main_car/ant_vision.py
@@ -355,7 +355,7 @@ class VisionManager:
                 dy = abs(self.real_servo_point[1] - self.last_real_servo_point[1])
                 self.last_car_x = self.my_car.x_current
                 self.last_car_y = self.my_car.y_current
-                
+
                 if (dx > MAX_POINT_CHANGE or dy > MAX_POINT_CHANGE):
                     if self.real_servo_point[1] - self.last_real_servo_point[1] < 0:
                         # 帧有效，更新记录
@@ -546,7 +546,6 @@ class VisionManager:
         if self.current_servo_object == 'T':
             self.my_plan.error_x = self.my_plan.error_x_T
             self.final_dist_y = self.servo_pid.target_y_T
-            self.final_dist_x = self.servo_pid.target_x_T
             if  current_turn_deg == -90:self.object_radius = self.radius_T_R
             elif current_turn_deg == 90:self.object_radius = self.radius_T_L
             elif current_turn_deg == 180:self.object_radius = self.radius_T_U
@@ -556,7 +555,6 @@ class VisionManager:
         elif self.current_servo_object in ['S', 'E']:
             self.my_plan.error_x = self.my_plan.error_x_S
             self.final_dist_y = self.servo_pid.target_y_S
-            self.final_dist_x = self.servo_pid.target_x_S
             if  current_turn_deg == -90:self.object_radius = self.radius_S_R
             elif current_turn_deg == 90:self.object_radius = self.radius_S_L
             elif current_turn_deg == 180:self.object_radius = self.radius_S_U
@@ -566,7 +564,6 @@ class VisionManager:
         elif self.current_servo_object in ['B', 'W']:
             self.my_plan.error_x = self.my_plan.error_x_B
             self.final_dist_y = self.servo_pid.target_y_B
-            self.final_dist_x = self.servo_pid.target_x_B
             if  current_turn_deg == -90:self.object_radius = self.radius_B_R
             elif current_turn_deg == 90:self.object_radius = self.radius_B_L
             elif current_turn_deg == 180:self.object_radius = self.radius_B_U
