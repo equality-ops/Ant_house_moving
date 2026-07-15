3726dac4e7e57d31bc13543edde5f75c73d1c351
LilAn09 <1327161394@qq.com>
2026-07-15 15:22:58 +0800

    feat: 调整摄像头亮度设置并重置保存图像编号格式

--- openmv/finish_competition/sample_collection.py
+++ openmv/finish_competition/sample_collection.py
@@ -16,7 +16,7 @@ sensor.set_framesize(sensor.QQVGA)
 sensor.set_framerate(60)
 sensor.set_auto_gain(False)  # 关闭自动增益
 sensor.set_auto_whitebal(False)  # 关闭自动白平衡
-sensor.set_brightness(800)
+sensor.set_brightness(600)
 sensor.set_contrast(2) # 对比度
 sensor.set_vflip(True)
 sensor.skip_frames(time=200)  # 跳过初始帧，让摄像头稳定
@@ -25,7 +25,7 @@ sensor.skip_frames(time=200)  # 跳过初始帧，让摄像头稳定
 clock = time.clock()
 
 
-save_img_num = 3485
+save_img_num = 0
 count = -100
 
 while(True):
@@ -37,7 +37,7 @@ while(True):
     if (count % 20) == 0 and count > 0:
         # 修改文件名称，准备保存
         save_img_num += 1
-        image_pat = "/sd/picture/"+str(save_img_num)+".jpg"
+        image_pat = "/sd/picture/"+str(save_img_num)+"_7_15.jpg"
 
         # 将拷贝之后的图像保存到sd卡
         img.save(image_pat,quality=99)
