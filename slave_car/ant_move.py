efc627f9c8421056ba275e1f0f4eb612be3dffe2
wangxuanxiang <3311597097@qq.com>
2026-08-13 22:31:20 +0800

    3
    3

--- slave_car/ant_move.py
+++ slave_car/ant_move.py
@@ -742,7 +742,10 @@ class MoveControl:
             if self.if_change_side:
                 self.my_plan.navigate(NAV_T['SLA_P'], NAV_T['ANGLE'][0],if_high_angle=False,if_first_turn=False)
             else:
-                self.my_plan.navigate(NAV_T['SLA_P'], NAV_T['ANGLE'][0],if_high_angle=True,if_first_turn=False)
+                if self.if_first_navigate:
+                    self.my_plan.navigate(NAV_T['SLA_P'], NAV_T['ANGLE'][0],if_high_angle=False,if_first_turn=False)
+                else:
+                    self.my_plan.navigate(NAV_T['SLA_P'], NAV_T['ANGLE'][0],if_high_angle=True,if_first_turn=False)
             if self.send_navigate_feed_back == False and self.my_plan.finished_dist >= 15:
                 self.send_navigate_feed_back = True
                 self.my_slave_protocol.send_slave_state("ready")
